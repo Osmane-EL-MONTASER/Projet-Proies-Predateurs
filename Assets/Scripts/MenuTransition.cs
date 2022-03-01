482f95adbae7aa9b5b27bd722e143233ab675eae
Pierre Averty <61978380+Shuitaa@users.noreply.github.com>
2022-03-01 08:54:08 +0100

    Ajout des transitions entre les menus

---
+++ Assets/Scripts/MenuTransition.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Classe codée par Pierre AVERTY pour la transition entre les scènes. 
+/// </summary>
+public class MenuTransition : MonoBehaviour
+{
+    /// <summary>
+    /// Objet parent du boutton cliqué.
+    /// </summary>
+    public GameObject parent;
+    
+    /// <summary>
+    /// Constructeur de la classe.
+    /// </summary>
+    public MenuTransition() {
+        
+    }
+
+    /// <summary>
+    /// Fonction qui cache et qui affiche un menu au clique.
+    /// 
+    /// Fait par Pierre AVERTY le 28/02/2022
+    /// </summary>
+    public void OnClick(GameObject newScene) {
+        parent = gameObject.transform.parent.gameObject;
+
+        parent.SetActive(false);
+        newScene.SetActive(true);
+    }
+
+}
