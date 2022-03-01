3015220d7e36e752338bc3b130adb7fff6b24cde
Osmane-cpp <63423818+Osmane-EL-MONTASER@users.noreply.github.com>
2022-03-01 21:40:31 +0100

    Ajout des commentaires dans LightningManager.cs

--- Assets/Scripts/Simulation/DayNightCycle/LightningManager.cs
+++ Assets/Scripts/Simulation/DayNightCycle/LightningManager.cs
@@ -13,8 +13,17 @@ public class LightningManager : MonoBehaviour {
     [SerializeField] private Light directionalLight;
     [SerializeField] private LightningPresets preset;
 
+    /// <summary>
+    /// L'heure actuelle dans le monde de la simulation.
+    /// </summary>
     [SerializeField, Range(0, 24)] private float timeOfDay;
 
+    /// <summary>
+    /// Pour faire avancer le temps dans le cycle jour /
+    /// nuit.
+    /// 
+    /// Intégrée par EL MONTASER Osmane le 01/03/2022. 
+    /// </summary>
     public void Update() {
         if(preset == null)
             return;
@@ -27,6 +36,15 @@ public class LightningManager : MonoBehaviour {
             updateLightning(timeOfDay / 24f);
     }
 
+    /// <summary>
+    /// Fonction qui permet de convertir le temps actuel en
+    /// un degré de rotation du soleil (le DirectionalLightning
+    /// de la scène).
+    /// 
+    /// Intégrée par EL MONTASER Osmane le 01/03/2022.
+    /// </summary>
+    /// <param name="timePercent">Le pourcentage du temps actuel 
+    /// par rapport à 24h.</param>
     private void updateLightning(float timePercent) {
         RenderSettings.ambientLight = preset.AmbientColor.Evaluate(timePercent);
         RenderSettings.fogColor = preset.FogColor.Evaluate(timePercent);
@@ -37,6 +55,10 @@ public class LightningManager : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Récupérer le premier DirectionalLightning sur lequel
+    /// travailler avec le cycle jour / nuit.
+    /// </summary>
     private void onValidate() {
         if(directionalLight != null)
             return;
