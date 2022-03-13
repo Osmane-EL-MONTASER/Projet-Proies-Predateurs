92c35c95962740c222e73235ef2b1ec411ffed47
Lotharim <greg.demirdjian@aim.com>
2022-03-13 18:51:50 +0100

    Update classe Agent

--- Assets/Scripts/Simulation/Agents/Agents.cs
+++ Assets/Scripts/Simulation/Agents/Agents.cs
@@ -13,69 +13,75 @@ using UnityEngine.UI;
 public class Agent : MonoBehaviour
 {
 
-    public NavMeshAgent agent;
+    public NavMeshAgent Agent;
 
-    private Terrain ter;
+    public Terrain Ter;
 
-    private Animator animation;
+    public Animator Animation;
 
-    private int portee;
+    public int Portee {get; set; }
 
-    private double besoinHydrique;
+    public double ApportEnergieCarcasse {get; set; }
 
-    private double besoinHydriqueMax;
+    private double _besoinHydrique;
 
-    private double besoinEnergie;
+    public double BesoinHydriqueMax {get; set; }
 
-    private double besoinEnergieMax;
+    private bool _aSoif;
 
-    private double coordX;
+    private double _besoinEnergie;
+
+    public double BesoinEnergieMax {get; set; }
+
+    private bool _aFaim;
+
+    private double _coordX;
     
-    private double coordY;
+    private double _coordY;
 
-    private double coordZ;
+    private double _coordZ;
 
-    private double vitesse;
+    private double _vitesse;
 
-    private double vitesseMax;
+    public double VitesseMax {get; set; }
 
-    private int sexe;
+    public int Sexe {get; set; }
 
-    private bool veutSeReprod;
+    private bool _veutSeReprod;
 
-    private bool estEnceinte;
+    private bool _estEnceinte;
 
-    private double tempsGrossesse;
+    public double TempsGrossesse {get; set; }
 
-    private double age;
+    public double Age {get; set; }
 
-    private double ageMaturation;
+    public double AgeMaturation {get; set; }
 
-    private double ageMax;
+    public double AgeMax {get; set; }
 
-    private bool estAdulte;
+    public bool EstAdulte {get; set; }
 
-    private int tempsDigestion;
+    public double TempsDigestion {get; set; }
 
-    private double tempsRestantDigestion;
+    private double _tempsRestantDigestion;
 
-    private int tempsConsoProie;
+    public double TempsConsoProie {get; set; }
 
-    private int pv;
+    public int Pv {get; set; }
 
-    private bool enVie;
+    public bool EnVie {get; set; }
 
-    private bool enFuite;
+    private bool _enFuite;
 
-    private int endurance;
+    private int _endurance;
 
-    private int enduranceMax;
+    public int EnduranceMax {get; set; }
 
-    private string causeDeces;
+    public string CauseDeces {get; set; }
 
-    private string nomEspece;
+    public string NomEspece {get; set; }
 
-    public int id;
+    public int Id {get; set; }
 
 
     /// <summary>
@@ -83,17 +89,20 @@ public class Agent : MonoBehaviour
     ///
     /// Fait par Greg Demirdjian le 12/03/2022.
     /// </summary> 
-    void initialisation()
+    private void initialisation()
     {
-        age = 0;
-        vitesse = 0.0;
-        veutSeReprod = false;
-        estEnceinte = false;
-        estAdulte = false;
-        enVie = true;
-        enFuite = false;
+        _besoinHydrique = 0.0;
+        _besoinEnergie = 0.0;
+        _vitesse = 0.0;
+        _veutSeReprod = false;
+        _estEnceinte = false;
+        Age = 0.0;
+        EstAdulte = false;
+        EnVie = true;
+        _enFuite = false;
+        _tempsRestantDigestion = 0.0;
 
-        //rajouter la recup d'info depuis la bdd
+        Agent.speed = _vitesse;
     }
 
     /// <summary>
@@ -103,9 +112,128 @@ public class Agent : MonoBehaviour
     /// </summary> 
     void Start()
     {
+        initialisation();
+    }
+
+    /// <summary>
+    /// Update : renverra vers les comportements en fonction des valeurs des variables de l'agent.
+    ///
+    /// Fait par Greg Demirdjian le 13/03/2022.
+    /// </summary>    
+    void Update()
+    {
+        testMort(); // teste si l'agent est en vie ou mort. modifie la variable EnVie
+
+        if (EnVie) // si l'agent est en vie, on peut lui appliquer des comportements.
+        {
+            _besoinHydrique+=0.15; // on augmente les besoins hydriques et énergétiques de l'agent.
+            _besoinEnergie+=0.1;
+
+            affecterComportement();
+            effectuerComportement();
+        }    
+        else
+        {
+            ApportEnergieCarcasse -= Time.deltaTime * 0.5; // la carcasse se déteriore et perd en apport énergétique.
+
+            if (ApportEnergieCarcasse<2.0) // si la carcasse est presque vide.
+                Destroy(this.gameObject); // on détruit l'objet.
+        }        
+
 
     }
 
+    /// <summary>
+    /// affecterComportement : teste si les variables de comportemnts doivent être changées.
+    ///
+    /// Fait par Greg Demirdjian le 12/03/2022.
+    /// </summary> 
+    private void affecterComportement()
+    {
+        if (_besoinHydrique/BesoinHydriqueMax>0.50) // si l'agent est à 50% de ses besoins hydriques max.
+            _aSoif = true;
+        if (_besoinEnergie/BesoinEnergieMax>0.70) // si l'agent est à 70% de ses besoins énergétiques max.
+            _aFaim = true;
+        if ((Age>=AgeMaturation)&&(EstAdulte==false)) // si l'agent dépasse l'age de maturation.
+            EstAdulte = true;
+    } 
 
+    /// <summary>
+    /// effectuerComportement : lance les fonctions de comportement si ceux-ci sont actifs.
+    ///
+    /// Fait par Greg Demirdjian le 13/03/2022.
+    /// </summary> 
+    private void effectuerComportement()
+    {
+        if (_enFuite)
+            Fuite();
+        else if (_aSoif)
+            Boire();
+        else if (_aFaim)
+            Manger();
+        
+    }
+
+    /// <summary>
+    /// testMort : teste si l'agent est mort.
+    ///
+    /// Fait par Greg Demirdjian le 13/03/2022.
+    /// </summary> 
+    private void testMort()
+    {
+        if (_besoinEnergie >= BesoinEnergieMax)
+        {
+            EnVie = false;
+            CauseDeces = "Mort de faim.";
+        }
+
+        if (_besoinHydrique >= BesoinHydriqueMax)
+        {
+            EnVie = false;
+            CauseDeces = "Mort de soif.";
+        }
+
+        if (Pv <= 0)
+        {
+            EnVie = false;
+            CauseDeces = "Plus de points de vie.";
+        }
+
+        if (Age >= AgeMax)
+        {
+            EnVie = false;
+            CauseDeces = "Mort de vieillesse.";
+        }
+    }
+
+    /// <summary>
+    /// Fuite : l'agent fuie. à écrire
+    ///
+    /// Fait par Greg Demirdjian le 13/03/2022.
+    /// </summary> 
+    void Fuite()
+    {
+
+    }
+
+    /// <summary>
+    /// Manger : l'agent cherche à se nourrir. Ici on doit distinguer les agents qui chassent de ceux qui se nourrissent d'autotrophes. à écrire
+    ///
+    /// Fait par Greg Demirdjian le 13/03/2022.
+    /// </summary> 
+    void Manger()
+    {
+
+    }
+
+    /// <summary>
+    /// Boire : l'agent cherche à boire. à écrire 
+    ///
+    /// Fait par Greg Demirdjian le 13/03/2022.
+    /// </summary> 
+    void Boire()
+    {
+
+    }
 
 }
