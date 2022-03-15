f9b61aa1b797b54b924e59d255e675a54a1dab3b
Bilal <71668544+BilalH-93@users.noreply.github.com>
2022-03-15 10:11:29 +0100

    Add files via upload

---
+++ Assets/Scripts/GUI/AfficherMenu.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+/// <summary>
+/// Classe codée par Bilal HAMICHE pour le prototype. Elle
+/// est insiprée d'une vidéo qui explique comment créer un menu d'options
+/// caméra. Brackeys, https://www.youtube.com/watch?v=wqm7iyoZSPI.
+/// </summary>
+public class AfficherMenu : MonoBehaviour
+{
+    /// <summary>
+    /// Variable de type GameObject sui permettra de gerer le menu options
+    /// </summary>
+    public GameObject MenuOptions;
+
+    /// <summary>
+    ///Variable de type bool qui est par defaut false
+    /// </summary>
+    bool visible = false;
+
+    /// <summary>
+    /// Methode pour detecter la touche Echap
+    /// </summary>
+    void Update()
+    {
+
+        /// <summary>
+        /// Si l'utilisateur appuie sur la touche Echap
+        /// le menu option va s'afficher
+        /// </summary>
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            visible = !visible;
+            MenuOptions.SetActive(visible);
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
