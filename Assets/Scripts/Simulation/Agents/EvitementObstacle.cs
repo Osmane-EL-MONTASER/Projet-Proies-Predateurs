0de15ef50d74d54ddd91420cf4c9bf88f5881152
Bilal <71668544+BilalH-93@users.noreply.github.com>
2022-04-11 16:29:35 +0200

    Update EvitementObstacle.cs

--- Assets/Scripts/Simulation/Agents/EvitementObstacle.cs
+++ Assets/Scripts/Simulation/Agents/EvitementObstacle.cs
@@ -1,3 +1,6 @@
+/*
+
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,40 +12,46 @@ using UnityEngine;
 /// 
 /// Fait par HAMICHE Bilal le 09/04/2022.
 /// </summary>
-public class EvitementObstacle : AgentAction {
-	
+public class EvitementObstacle : AgentAction
+{
+
 	private CharacterController controller;
-	
+
 	private float verticalVelocity;
 	private float gravity = 14.0f;
 	private float jumpForce = 10.0f;
-	
-	
-	private void Start() {
+
+
+	private void Start()
+	{
 		controller = GetComponent<CharacterController>();
 	}
-	
-	
-	
-	
+
+
+
+
 	// ou private
-	public override void update() {
-		if(controller.isGrounded)
+	public override void update()
+	{
+		if (controller.isGrounded)
 		{
 			verticalVelocity = -gravity * Time.deltaTime;
-			if(Input.GetKeyDown(KeyCode.Space)) {
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
 				verticalVelocity = jumpForce;
-				
+
 			}
-			else {
+			else
+			{
 				verticalVelocity -= gravity * Time.deltaTime;
-				}
-				
-			Vector3 moveVector = new Vector3(0,verticalVelocity,0);
+			}
+
+			Vector3 moveVector = new Vector3(0, verticalVelocity, 0);
 			controller.Move(moveVector * Time.deltaTime);
-	
+
 		}
 
 	}
-	
+
 }
+*/
