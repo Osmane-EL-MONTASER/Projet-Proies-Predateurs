a866dcc6f7c4654730305745ce78081429677c91
Bilal <71668544+BilalH-93@users.noreply.github.com>
2022-04-17 18:02:41 +0200

    Add files via upload

---
+++ Assets/Scripts/GUI/QuitterSimulation.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Classe codée par Bilal HAMICHE le 17/04 pour le prototype. Elle permet la création de quitter la simulation.
+/// </summary>
+public class QuitterSimulation : MonoBehaviour
+{
+	public void QuiiterSimulation()
+	{
+		
+		Application.Quit();
+		Debug.Log("Fin de la simulation");
+	}
+}
