bb7fc27c65e69ae3710c70bdbcb31c708498a9bf
Osmane-cpp <63423818+Osmane-EL-MONTASER@users.noreply.github.com>
2022-05-08 18:29:02 +0200

    Revert "Revert "merge""
    This reverts commit b2a0819c4464a134e0204ea9d78c466bfde4a376.

--- Assets/DataDiagram/Editor/DD_Menu.cs
+++ Assets/DataDiagram/Editor/DD_Menu.cs
@@ -4,6 +4,11 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
+
+/// <summary>
+/// Classe reprise par Bilal HAMICHE pour le prototype. 
+/// Elle permet de créer le menu qui affiche le graphe
+/// </summary>
 public class DD_Menu : MonoBehaviour {
 
     // Use this for initialization
@@ -16,8 +21,6 @@ public class DD_Menu : MonoBehaviour {
 		
 	}
 
-    ///如果要让Hierarchy里面的Gameobject通过鼠标右键单击
-    ///弹出对话框中出现该选项,则需要将该选项加入到"GameObject"目录下
     [MenuItem("GameObject/UI/DataDiagram")]
     public static void AddDataDiagramInGameObject() {
 
@@ -31,7 +34,7 @@ public class DD_Menu : MonoBehaviour {
         if ((null == parent) || (null == parent.GetComponentInParent<Canvas>())) {
             Canvas canvas = FindObjectOfType<Canvas>();
             if(null == canvas) {
-                Debug.LogError("AddDataDiagram : can not find a canvas in scene!");
+                Debug.LogError("AddDataDiagram : Impossible de trouver un canvas dans la scene!");
                 return;
             } else {
                 parent = FindObjectOfType<Canvas>().gameObject;
@@ -40,9 +43,13 @@ public class DD_Menu : MonoBehaviour {
         
         GameObject prefab = Resources.Load("Prefabs/DataDiagram") as GameObject;
         if (null == prefab) {
-            Debug.LogError("AddDataDiagram : Load DataDiagram Error!");
+            Debug.LogError("AddDataDiagram : Erreur de chargement de DataDiagram!");
             return;
         }
+        /// <summary>
+        /// Variable de type GameObject 
+        /// 
+        /// </summary>
 
         GameObject dataDiagram;
         if (null != parent)
@@ -51,7 +58,7 @@ public class DD_Menu : MonoBehaviour {
             dataDiagram = Instantiate(prefab);
 
         if(null == dataDiagram) {
-            Debug.LogError("AddDataDiagram : Instantiate DataDiagram Error!");
+            Debug.LogError("AddDataDiagram : Erreur lors de l'instanciation de DataDiagram");
             return;
         }
 
